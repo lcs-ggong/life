eb9f0131796f8f6b506b8280d825433e3719a485
Grace Gong <ggong@lcs.on.ca>
2023-05-25 15:11:34 -0400

    Initial Commit

---
+++ life/lifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeApp.swift
+//  life
+//
+//  Created by Grace Gong on 2023-05-25.
+//
+
+import SwiftUI
+
+@main
+struct lifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
